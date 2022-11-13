6006f64dce61bb14178325233c3f654da74d62f3
osawa-koki <github@osawa.dev>
2022-11-14 00:03:59 +0900

    simple greeting

---
+++ src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
@@ -0,0 +1,18 @@
+package com.kotlinspring.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingController {
+
+  @GetMapping("/{name}")
+  fun retrieveGreetings(@PathVariable name: String): String {
+    return "Hello, $name!"
+  }
+
+
+}
