2fc36d606e2c6671f4fb338879c6bf54994a4578
osawa-koki <github@osawa.dev>
2022-11-14 00:21:00 +0900

    VM Optionsの制御コメントを追加。

--- src/main/kotlin/com/kotlinspring/CourceCatalogServiceApplication.kt
+++ src/main/kotlin/com/kotlinspring/CourceCatalogServiceApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class CourceCatalogServiceApplication
 
+// VM Options: -Dspring.profiles.active=prod
+// を設定することで、application-prod.ymlの値を制御する。
 fun main(args: Array<String>) {
 	runApplication<CourceCatalogServiceApplication>(*args)
 }
