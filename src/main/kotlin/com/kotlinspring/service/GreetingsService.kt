402ed69ca341b3ec3bfdf70dfc0fcfbb16783f4d
osawa-koki <github@osawa.dev>
2022-11-14 00:15:48 +0900

    application.ymlファイルの作成と使用。

--- src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
@@ -1,12 +1,17 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsService() {
 
+  // application.ymlの値を使用する。
+  @Value("\${message}")
+  lateinit var message: String
+
   fun retrieveGreetings(name: String): String {
-    return "Hello, $name!"
+    return "$message, $name!"
   }
 
 }
